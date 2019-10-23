7d3ff7ab03343df502897d10f483986de0fb0246
Jamaal Ahmed <jamaal729@yahoo.com>
2019-10-23 20:57:21 +0000

    Implemented snake movements

--- client.js
+++ client.js
@@ -1,8 +1,6 @@
 const net = require('net');
 
 //  Move the connect function from play.js into client.js, where it should be exported.
-
-
 /**
  * Establishes connection with the game server
  */
@@ -12,21 +10,48 @@ const connect = function () {
     host: 'localhost',
     port: 50541
   });
+
   // interpret incoming data as text
   conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+   
+  });
+
+  conn.on('connect', () => {
+    conn.write('Move: up');
+
+  });
 
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write("Name: JJA");
+
+    let m = setInterval(() => {
+      conn.write("Move: up");
+      conn.write("Move: right");
+
+    }, 50);
+    setTimeout(() =>  clearInterval (m), 2000);
   });
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-
 
   return conn;
+}
+
+//  * Setup User Interface 
+//  * Specifically, so that we can handle user input via stdin
+//  */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+
+const handleUserInput = function() {
+
 }
 
 module.exports = connect;
